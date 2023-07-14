cef688383500318312a7da399aeb09fddfe42422
DanilaAneichik <danila.aneychik2005@gmail.com>
2023-07-14 21:45:25 +0300

    1. Create a coin pick up logic.

--- Assets/Scripts/Definitions/Tags.cs
+++ Assets/Scripts/Definitions/Tags.cs
@@ -3,5 +3,6 @@
     public static class Tags
     {
         public const string Joystick = "Joystick";
+        public const string Player = "Player";
     }
 }
--- Assets/Scripts/Objects/Coin.cs
+++ Assets/Scripts/Objects/Coin.cs
@@ -1,7 +1,25 @@
-﻿namespace Objects
+﻿using Definitions;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Objects
 {
-    public class Coin
+    public class Coin : MonoBehaviourPun
     {
-        
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag(Tags.Player))
+            {
+                Debug.Log("Collision detected");
+                CoinsScreen.CoinsAmount++;
+                GetComponent<PhotonView>().RPC("CoinCollected", RpcTarget.AllBuffered);
+            }
+        }
+
+        [PunRPC]
+        private void CoinCollected()
+        {
+            Destroy(gameObject);
+        }
     }
 }

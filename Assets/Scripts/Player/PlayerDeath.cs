6a17f0f7f731839a42d5376dd5ebc5bef61c7f2b
DanilaAneichik <danila.aneychik2005@gmail.com>
2023-07-15 15:15:01 +0300

    1. Create a place for name.

---
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerDeath : MonoBehaviour
+    {
+        [Header("Player component to disable")]
+        [SerializeField] private PlayerHp _playerHp;
+        [SerializeField] private PlayerMovement _playerMovement;
+        [SerializeField] private PlayerAttack _playerAttack;
+
+        public bool IsDead { get; private set; }
+
+        private void Start()
+        {
+            _playerHp.OnChanged += OnHpChanged;
+        }
+
+        private void OnHpChanged(int hp)
+        {
+            if (IsDead || hp > 0)
+                return;
+
+            IsDead = true;
+            Debug.Log("Player is dead");
+            _playerMovement.enabled = false;
+            _playerAttack.enabled = false;
+        }
+    }
+}
